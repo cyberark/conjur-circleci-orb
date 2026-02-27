220776df422a4a220220855dffaadbd9ef6c7858
Itso-Dimitrov-CyberArk <itso.dimitrov@cyberark.com>
2026-02-27 11:17:09 +0200

    Moved Base Path of the Submodule to Variable

--- bin/integration_test_coverage.sh
+++ bin/integration_test_coverage.sh
@@ -2,13 +2,14 @@
 set -euo pipefail
 
 # Using helpers from the secrets-manager-integration-environment-bootstrap repo to setup the environment for integration tests
-source "./secrets-manager-integration-environment-bootstrap/scripts/helpers.sh"
+SUBMODULE_BASE_DIR=secrets-manager-integration-environment-bootstrap
+source "./$SUBMODULE_BASE_DIR/scripts/helpers.sh"
 
 validate_deployment_type "$@"
 set_compose_file
 
 # Load config to set PROFILE environment variable
-CONFIG_FILE="./secrets-manager-integration-environment-bootstrap/configs/${INTEGRATION_TYPE}.env"
+CONFIG_FILE="./$SUBMODULE_BASE_DIR/configs/${INTEGRATION_TYPE}.env"
 if [[ -f "$CONFIG_FILE" ]]; then
     set -a  # Export all subsequent variable assignments
     source "$CONFIG_FILE"
@@ -22,7 +23,7 @@ fi
 # Always cleanup containers on exit
 cleanup() {
 	echo "[CLEANUP] Stopping and removing containers and volumes from docker-compose.yml."
-	docker compose -f "$COMPOSE_FILE" -f secrets-manager-integration-environment-bootstrap/docker-compose/common-services.yml --profile "$PROFILE" down -v
+	docker compose -f "$COMPOSE_FILE" -f "$SUBMODULE_BASE_DIR/docker-compose/common-services.yml" --profile "$PROFILE" down -v
 }
 trap cleanup EXIT ERR INT
 
@@ -30,12 +31,12 @@ OUTPUT_DIR="./output-integration"
 
 mkdir -p "$OUTPUT_DIR"
 
-./secrets-manager-integration-environment-bootstrap/scripts/setup_integration_env.sh "$DEPLOYMENT_TYPE" "$INTEGRATION_TYPE"
+./$SUBMODULE_BASE_DIR/scripts/setup_integration_env.sh "$DEPLOYMENT_TYPE" "$INTEGRATION_TYPE"
 
 docker build -f dockerintegration/Dockerfile.integration -t integration-test .
 docker run  --rm \
 	-v "$OUTPUT_DIR:/conjur-circleci-orb/output-integration" \
-	-v "$(pwd)/secrets-manager-integration-environment-bootstrap/configs:/configs:rw" \
+	-v "$(pwd)/$SUBMODULE_BASE_DIR/configs:/configs:rw" \
 	-e "CONJUR_URL=${CONJUR_URL}" \
 	-e "DEPLOYMENT_TYPE=${DEPLOYMENT_TYPE}" \
 	-e "API_KEY=${API_KEY}" \
